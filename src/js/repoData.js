9455d5a1f300cb7ec3058747ad797e0eb308bf7b
Jonatha Fernandes <jonathafernandes@live.com>
2024-08-29 14:35:21 -0300

    refactor search and show of repos

--- src/js/repoData.js
+++ src/js/repoData.js
@@ -1,50 +1,81 @@
+async function fetchData(url) {
+    try {
+        const response = await axios.get(url);
+        const allRepositories = response.data;
+
+        const nextLink = response.headers.link &&
+                         response.headers.link.split(',')
+                               .find(link => link.includes('rel="next"'));
+
+        if (nextLink) {
+            const nextUrl = nextLink.split(';')[0].slice(1, -1);
+            const nextData = await fetchData(nextUrl);
+            return allRepositories.concat(nextData);
+        }
+        return allRepositories;
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error.message);
+        throw error;
+    }
+}
+
 async function getSpecificRepositories() {
     try {
         const username = 'jonathafernandes';
         const perPage = 100;
-        let allRepositories = [];
-
-        const fetchData = async (url) => {
-            const response = await axios.get(url);
-            allRepositories = allRepositories.concat(response.data);
-
-        const nextLink = response.headers.link && response.headers.link.split(',').find(link => link.includes('rel="next"'));
-        if (nextLink) {
-            const nextUrl = nextLink.split(';')[0].slice(1, -1);
-            await fetchData(nextUrl);
-        }
-        };
-
         const initialUrl = `https://api.github.com/users/${username}/repos?per_page=${perPage}`;
-        await fetchData(initialUrl);
+        const allRepositories = await fetchData(initialUrl);
 
-        const specificRepositories = ["monteiro-esportes-cms","blog.github.io"];
-        const filteredRepositories = allRepositories.filter(repo => specificRepositories.includes(repo.name));
+        const repoBlog = allRepositories.find(repo => repo.name === 'blog.github.io');
+        const latestRepo = allRepositories
+            .filter(repo => repo.topics.length > 0)
+            .reduce((a, b) => new Date(a.created_at) > new Date(b.created_at) ? a : b, {});
 
-        const lastProject = document.getElementById('lastProject');
-        const lastProjectData = filteredRepositories[1];
+        const specificRepositories = [repoBlog, latestRepo];
 
+        renderRepositories(specificRepositories);
+    } catch (error) {
+        renderError('Erro ao obter repositório!');
+    }
+}
+
+function renderRepositories(repositories) {
+    const lastProject = document.getElementById('lastProject');
+    const articleContent = document.getElementById('article-content');
+
+    if (repositories[1]) {
         lastProject.innerHTML = `
-            <h5>${lastProjectData.name}</h5>
-            <img src="./src/assets/${specificRepositories[0]}.png" alt="">
+            <h5>${[1].name || 'Repositório'}</h5>
+            <img src="./src/assets/${repositories[1].name}.png" alt=${repositories[1].name}>
             <br>
-            <span>${lastProjectData.description}<span>
+            <span>${[1].description || ''}<span>
             <div class="repo-buttons">
-                <a href="${lastProjectData.html_url}" target="_blank">Mais informações</a>
-                <a href="${lastProjectData.homepage}" target="_blank">Visite</a>
+                <a href="${[1].html_url || '#'}" target="_blank">Mais informações</a>
+                <a href="${[1].homepage || '#'}" target="_blank">Visite</a>
             </div>
         `;
+    } else {
+        lastProject.innerHTML = `<p style="color:darkred">Repositório não encontrado.</p>`;
+    }
 
-        const articleContent = document.getElementById('article-content');
-        const articleData = filteredRepositories[0];
+    if (repositories[0]) {
         articleContent.innerHTML = `
-            <a href="${articleData.homepage}" target="_blank">${articleData.description}</a>
+            <a href="${repositories[0].homepage || '#'}" target="_blank">${repositories[0].description || 'Descrição do repositório'}</a>
         `;
+    } else {
+        articleContent.innerHTML = `<p style="color:darkred">Artigo não encontrado.</p>`;
+    }
+}
 
-    } catch (error) {
-        console.error('Erro ao obter repositório:', error.message);
-        lastProject.innerHTML = `<p style="color: red">Erro ao obter repositório!</p>`
-        lastPost.innerHTML = `<p style="color: red">Erro ao obter informações do post!</p>`
+function renderError(message) {
+    const lastProject = document.getElementById('lastProject');
+    const articleContent = document.getElementById('article-content');
+
+    if (lastProject) {
+        lastProject.innerHTML = `<p style="color:darkred">${message}</p>`;
+    }
+    if (articleContent) {
+        articleContent.innerHTML = `<p style="color:darkred">Erro ao obter informações do post!</p>`;
     }
 }
 
